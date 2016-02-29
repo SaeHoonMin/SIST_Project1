083ec38137fcfc0594ec5cfe9f239bda75771b6b
idryu2 <idryu2@naver.com>
2016-02-29 10:20:17 +0900

    Hello java

---
+++ IdeaProjects/HelloJava/src/com/sist/HelloJava.java
@@ -0,0 +1,11 @@
+package com.sist;
+
+/**
+ * Created by aaa on 2016-02-29.
+ */
+public class HelloJava {
+
+    public static void main(String[] args){
+        System.out.println("Hello java");
+    }
+}
