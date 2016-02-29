3d64baa44084b4ecae79c88b1ec2217882f8c7d3
idryu2 <idryu2@naver.com>
2016-02-29 10:15:20 +0900

    Hello java

---
+++ IdeaProjects/Annotation/src/com/sist/MethodUseAnnotation.java
@@ -0,0 +1,17 @@
+package com.sist;
+import java.lang.annotation.ElementType;
+
+/**
+ *
+ *
+ *
+ */
+public class MethodUseAnnotation {
+    @PrintAnnotation
+    public void methoName1(){
+        System.out.println("어노테이션 적용 메서드1 내용");
+
+    }
+
+
+}
